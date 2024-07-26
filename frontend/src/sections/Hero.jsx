40afc62480aa2157e6ae17965d8d344d53be60f0
rossiKellss <rossi.dev19@gmail.com>
2024-07-26 21:27:12 +0545

    modifying hero section with swiper

--- frontend/src/sections/Hero.jsx
+++ frontend/src/sections/Hero.jsx
@@ -2,17 +2,45 @@ import React from "react";
 import Heading from "../components/SubComponent/HeadingTitle/Heading";
 import Para from "../components/SubComponent/Paragraph/Para";
 import Button from "../components/SubComponent/Button/Button";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+import 'swiper/css'
+import "swiper/css/pagination";
 
 function Hero() {
   return (
-    <div className="w-full rounded-lg p-4 mt-28 md:mt-32 lg:mt-36 bg-gray-500 flex md:py-8 lg:py-14 lg:pt-16 lg:px-8 lg:h-[72vh]">
-      <div className="md:w-[75%] space-y-1 md:space-y-2">
-        <Heading content={"We provide best veges"} />
-        <div className="flex flex-col gap-2 lg:gap-3 justify-start items-start">
-          <Para />
-          <Button />
-        </div>
-      </div>
+    <div className=" w-full rounded-lg  mt-28 md:mt-32 lg:mt-36 bg-gray-500 flex md:py-0  lg:h-[72vh]">
+      <Swiper
+        slidesPerView={1}
+        
+        pagination={{
+         
+
+          clickable: true,
+          // renderBullet: (index, className) => {
+          //   return `<span class="${className} "/></span>`;
+          // },
+        }}
+        modules={[Pagination]}
+      >
+        <SwiperSlide>
+          <div className="w-full">
+            swipr
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div className="w-full rounded-lg    space-y-1 md:space-y-2 bg-[url('https://images.unsplash.com/photo-1471194402529-8e0f5a675de6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHZlZ2V0YWJsZXxlbnwwfHwwfHx8MA%3D%3D')]">
+           
+            <Heading content={"We provide best veges"} />
+            <div className="flex flex-col gap-2 lg:gap-3 justify-start items-start">
+              <Para />
+              <Button />
+            </div>
+          </div>
+        </SwiperSlide>
+       
+      </Swiper>
     </div>
   );
 }
