0a9428daedca84d2d7b671024b6d5adc2379591e
rossiKellss <rossi.dev19@gmail.com>
2024-09-18 13:54:43 +0545

    password validation almost done

--- frontend/src/pages/AuthPages/ChangePassword.jsx
+++ frontend/src/pages/AuthPages/ChangePassword.jsx
@@ -5,25 +5,65 @@ import { PiEyeBold } from "react-icons/pi";
 import Button from "../../components/SubComponent/Button/Button";
 
 function ChangePassword() {
-
   const [showPassword, setShowPassword] = useState({
-    password: false,
     changePassword: false,
+    confirmPassword: false,
   });
-  const [errors,setErrors]=useState({});
+  const [errors, setErrors] = useState({});
+  const [getPassword, setPassword] = useState({});
+
+  
+
+  const validate = () => {
+    let errors = {};
+    if (!getPassword.changePassword) {
+      errors.changePassword = "Enter the password!";
+    } else if (getPassword.changePassword.length < 8) {
+      errors.changePassword = "Password must be at least 8 characters long!";
+    }
+
+    if (!getPassword.confirmPassword) {
+      errors.confirmPassword = "Enter the password!";
+    } else if (getPassword.confirmPassword.length < 8) {
+      errors.confirmPassword = "Password must be at least 8 characters long!";
+    }
+
+    if (getPassword.changePassword != getPassword.confirmPassword) {
+      errors.confirmPassword = "Password doesn't match!";
+    }
+
+    return errors;
+  };
+
+  const countErrors = () => {
+    const errLen = Object.entries(errors).filter(
+      ([key, value]) => !value == ""
+    );
+    return errLen;
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors(validate(getPassword));
+  };
   return (
     <div className="w-[80%] mx-auto mt-28 md:mt-26 lg:mt-28">
       <Heading content={"Change password"} />
-      <form action="" className="mt-2">
+      <form action="" className="mt-2" onSubmit={handleSubmit}>
         <div className="w-full  flex flex-col gap-6 mb-2 ">
-          <div className="flex border-b-2">
+          <div className="flex flex-col">
+            <div className="flex border-b-2">
+
+            
             <input
               type={`${showPassword.changePassword ? "text" : "password"}`}
               className="w-full outline-none tracking-wide "
               placeholder="Enter your new password"
               name="changePassword"
               onChange={(e) => {
-                getUserCred(e);
+                setPassword({
+                  ...getPassword,
+                  [e.target.name]: e.target.value,
+                });
               }}
             />
 
@@ -50,41 +90,51 @@ function ChangePassword() {
                 }}
               />
             )}
+            </div>
+            <p className="text-red-500 text-sm">{errors?.changePassword}</p>
           </div>
-          <div className="flex border-b-2">
-            <input
-              type={`${showPassword.changePassword ? "text" : "password"}`}
-              className="w-full outline-none tracking-wide "
-              placeholder="Confirm your password"
-              name="changePassword"
-              onChange={(e) => {
-                getUserCred(e);
-              }}
-            />
-
-            {!showPassword.changePassword && (
-              <PiEyeClosedBold
-                className="text-gray-500 text-lg"
-                onClick={() => {
-                  setShowPassword({
-                    ...showPassword,
-                    changePassword: !showPassword.changePassword,
+          <div className="flex flex-col gap-2 ">
+            <div className="flex border-b-2">
+              <input
+                type={`${showPassword.confirmPassword ? "text" : "password"}`}
+                className="w-full outline-none tracking-wide "
+                placeholder="Confirm your password"
+                name="confirmPassword"
+                onChange={(e) => {
+                  setPassword({
+                    ...getPassword,
+                    [e.target.name]: e.target.value,
                   });
                 }}
               />
-            )}
 
-            {showPassword.changePassword && (
-              <PiEyeBold
-                className="text-gray-500 text-lg"
-                onClick={() => {
-                  setShowPassword({
-                    ...showPassword,
-                    changePassword: !showPassword.changePassword,
-                  });
-                }}
-              />
-            )}
+              {!showPassword.confirmPassword && (
+                <PiEyeClosedBold
+                  className="text-gray-500 text-lg"
+                  onClick={() => {
+                    setShowPassword({
+                      ...showPassword,
+                      confirmPassword: !showPassword.confirmPassword,
+                    });
+                  }}
+                />
+              )}
+
+              {showPassword.confirmPassword && (
+                <PiEyeBold
+                  className="text-gray-500 text-lg"
+                  onClick={() => {
+                    setShowPassword({
+                      ...showPassword,
+                      confirmPassword: !showPassword.confirmPassword,
+                    });
+                  }}
+                />
+              )}
+            </div>
+            <p className="text-red-500 text-sm">
+              {errors?.confirmPassword}
+            </p>
           </div>
         </div>
         <div className="w-full mt-6 md:w-[20%] lg:w-[12%]">
